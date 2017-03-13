758b9d68b8a0e1e710d4d22202ccd5c66c3e8463
Besto <manofthebag@gmail.com>
2017-03-13 13:49:17 -0400

    [Update] example

--- example.js
+++ example.js
@@ -1,3 +1,16 @@
 const User = require('.');
 
-bot = new User();
+bot = new User({
+  accessToken: ''
+});
+
+bot.sendMessage(237799109, "Well, *hello* there!", {parse_mode: 'markdown'})
+   .then(res=>bot.sendMessage({chat_id: '237799109', text: 'The text succeeded, Master!'})
+   .catch(err=>console.log(err))
+
+bot.sendMessage({
+  chatId: '',
+  text: 'Hello world!'
+}, (error, ctx) => {
+  console.log(ctx)
+});
